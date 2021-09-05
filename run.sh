1edcc9f3ad304baa57901553b1351d977f918606
Jayson Helseth <jayson.helseth@gmail.com>
2021-09-05 18:53:02 -0500

    Changed how the server is started.

--- run.sh
+++ run.sh
@@ -2,9 +2,5 @@
 
 SERVER="$HOME/projects/SensorSend/backend/server.py"
 
-/usr/bin/python3 "$SERVER" &
-S_ID=$!
-
+sudo systemctl start sensorsend
 /usr/bin/chromium-browser --kiosk "file:///$HOME/projects/SensorSend/ui/index.html"
-
-kill $S_ID
